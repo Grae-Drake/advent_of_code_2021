615ae363ec06e72d0075f3e45c47c0d9ca82f989
Grae Drake <graemdrake@gmail.com>
2021-12-14 11:11:14 -0800

    Solve Day14a

---
+++ day14a.py
@@ -0,0 +1,73 @@
+import argparse
+from collections import defaultdict
+
+
+def get_data(data_path):
+    with open(data_path) as file:
+        seed, raw_rules = file.read().split('\n\n')
+        seed = seed.rstrip()
+        rules = {}
+        for raw_rule in raw_rules.rstrip().split('\n'):
+            x, y = raw_rule.split(' -> ')
+            rules[x] = y
+        
+        return (seed, rules)
+
+
+def pairs(seed):
+    return [''.join(pair) for pair in zip(seed[:-1], seed[1:])]
+
+
+def main(data_path):
+        seed, rules = get_data(data_path)
+        for x in range(10):
+            daughters = []
+            for pair in pairs(seed):
+                daughters.append(rules[pair])
+            new_seed = seed[0]
+            for i, char in enumerate(seed[1:]):
+                new_seed += daughters[i] + char
+            seed = new_seed
+        
+        element_counts = defaultdict(int)
+        for element in seed:
+            element_counts[element] += 1
+
+        return max(element_counts.values()) - min(element_counts.values())
+
+        
+
+
+
+
+
+
+
+
+        # Iterate.
+        for x in range(9):
+            new_pair_counts = defaultdict(int)
+            for pair, count in pair_counts.items():
+                daughters = rules[pair]
+                for daughter in daughters:
+                    new_pair_counts[daughter] += count
+            pair_counts = new_pair_counts
+
+        # Get individual element counts.
+        element_counts = defaultdict(int)
+        for pair, count in pair_counts.items():
+            for element in pair:
+                element_counts[element] += count
+        print(element_counts)
+
+        return max(element_counts.values()) - min(element_counts.values())
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Day 14')
+    parser.add_argument('-t', dest='test', action='store_true',
+                        help='use test data (default: use real data)')
+    args = parser.parse_args()
+    day = 14
+    data_path = 'data/day' + str(day) + ('_test' if args.test else '') + '.txt'
+    print(main(data_path))
