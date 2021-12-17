763dcb9767219ffddfc0794fd096680f07bd7f4a
Grae Drake <graemdrake@gmail.com>
2021-12-17 12:36:09 -0800

    Solve Day16b

--- day16b.py
+++ day16b.py
@@ -1,4 +1,5 @@
 import argparse
+import math
 
 
 def get_data(data_path):
@@ -15,6 +16,24 @@ def hex_to_bin(hex_str):
     return leftpad(raw_binary, len(hex_str) * 4)
 
 
+def value(packet):
+    operation = packet.type_ID
+    operations = [
+        lambda arguments: sum(arguments),
+        lambda arguments: math.prod(arguments),
+        lambda arguments: min(arguments),
+        lambda arguments: max(arguments),
+        None,
+        lambda arguments: 1 if arguments[0] > arguments[1] else 0,
+        lambda arguments: 1 if arguments[0] < arguments[1] else 0,
+        lambda arguments: 1 if arguments[0] == arguments[1] else 0
+    ]
+    if operation == 4:
+        return packet.literal
+    else:
+        return operations[operation]([value(child) for child in packet.children])
+
+
 class Packet():
 
     def __init__(self, binary):
@@ -23,12 +42,7 @@ class Packet():
         self.children = []
         
         if self.type_ID != 4:
-            print("Handling an operator. Packet info:")
-            print("Packet bits:\n{}".format(binary))
-            print("Version: {}".format(self.version))
-            print("type_ID: {}".format(self.type_ID))
             self.length_type_ID = int(binary[6:7])
-            print("length_type_ID: {}".format(self.length_type_ID))
             pointer = 7
             
             # Handle operators with a set length.
@@ -36,8 +50,6 @@ class Packet():
                 sub_packets_length = int(binary[pointer: pointer + 15], 2)
                 pointer += 15
                 sub_packets_end = pointer + sub_packets_length
-                print('Sub Packet(s) length: {}'.format(sub_packets_length))
-                print("Sub packet(s) binary: {}".format(binary[pointer: sub_packets_end]))
                 while pointer < sub_packets_end:
                     child = Packet(binary[pointer: sub_packets_end])
                     pointer += child.length
@@ -54,30 +66,17 @@ class Packet():
         
         # Handle literals
         else:
-            print('Handling a literal. Packet info:')
-            print(binary)
-            print("Version: {}".format(self.version))
-            print("type_ID: {}".format(self.type_ID))
             pointer = 6
-            print("Literal bits:\n{}".format(binary[pointer:]))
             bits = []
             end = 1
             while end:
-                print("Bits: {}".format(bits))
                 end = int(binary[pointer])
-                print(end)
                 for i in range(1, 5):
                     bits.append(binary[pointer + i])
                 pointer += 5
             self.literal = int(''.join(bits), 2)
-            print("Literal value: {}".format(self.literal))
-            # Need to figure out how to handle the "extra" zeros at the end of a literal.
 
         self.length = pointer
-
-
-def packet_version_sum(packet):
-    return packet.version + sum([packet_version_sum(child) for child in packet.children])
 
 
 def main(data_path):
@@ -85,10 +84,9 @@ def main(data_path):
 
     results = []
     for message in bin_data:
-        print("\n------ New Test ------")
         packet = Packet(message)
         results.append(packet)
-    return [packet_version_sum(result) for result in results]
+    return [value(result) for result in results]
 
 
 if __name__ == '__main__':
